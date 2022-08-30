4d1c418d4fff0d8c19f6dafed16c572badb58cf9
GeorgianaBarbu1 <georgiana.barbu304@yahoo.com>
2022-08-30 18:50:51 +0300

    adding changes

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-//facem o functie pt functionalitatea site-ului-pentru countdown
-
 const countdown = () => {
-    const countDate = new Date("June 29, 2021 00:00:00").getTime();
+   const countDate = new Date("June 29, 2021 00:00:00").getTime();
    const now = new Date().getTime();
    const gap = countDate - now;
 
-   //how the time works
    const second = 1000;
    const minute = second * 60;
    const hour = minute * 60;
    const day = hour * 24;
 
-   //calculate from the gat how many minutes/sec are there
    const textDay =Math.floor( gap / day );
    const textHour = Math.floor((gap % day) / hour);
    const textMinute = Math.floor((gap % hour) / minute);
